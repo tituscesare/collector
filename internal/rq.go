01d40f38d35f92619012b4354643aa322a1b88ae
tituscesare <47405702+tituscesare@users.noreply.github.com>
2023-05-01 11:44:36 +0300

    Update rq.go

--- internal/rq.go
+++ internal/rq.go
@@ -19,20 +19,43 @@ func updateCounter(w http.ResponseWriter, r *http.Request){
      
    if r.Method != http.MethodPost {
       
+      http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
       return
    
    } else {
    
      tp, err := r.URL.Query().Get("tp") // param type
-     nm, err := r.URL.Query().Get("nm") // param name
-     vl,err  := r.URL.Query().Get("vl") // param value
      
+      // При попытке передать запрос с некорректным типом метрики возвращать http.StatusBadRequest. 
+      
+      if err != nil {
+         
+         http.Error(w, "mertic type is incorrect", http.StatusBadRequest)
+         
+         return
+      }
+      
+      nm, err := r.URL.Query().Get("nm") // param name
+      
+      // При попытке передать запрос без имени метрики возвращать http.StatusNotFound.
+      if err != nil {
+         
+         http.Error(w, "mertic name is empty", http.StatusNotFound)
+         
+         return
+      }
+      
+     vl, err := r.URL.Query().Get("vl") // param value
      
+      // При попытке передать запрос с некорректным значением возвращать http.StatusBadRequest. 
+     if err != nil {
+         
+         http.Error(w, "mertic value is incorrect", http.StatusBadRequest)
+         
+         return
+      }
+      
      // При успешном приёме возвращать http.StatusOK.
-
-     // При попытке передать запрос без имени метрики возвращать http.StatusNotFound.
-
-     //При попытке передать запрос с некорректным типом метрики или значением возвращать http.StatusBadRequest.
  
    }     
 }
