bf6e0738a2ac7e27846c6d2a25354c2b4cec0d68
tituscesare <47405702+tituscesare@users.noreply.github.com>
2023-05-01 15:00:22 +0300

    Update wb.go

--- internal/wb/wb.go
+++ internal/wb/wb.go
@@ -5,6 +5,7 @@ import (
    
    "log" 
    "strings"	
+   "strconv"
    "net/http"
 )
 
@@ -25,14 +26,33 @@ func updateCounter(w http.ResponseWriter, r *http.Request){
    } else {
 	   
 	   paramUrl := r.URL.RequestURI()
+	  
+	   serviceName := strings.Split(paramUrl,"/")[1] // имя сервиса
 	   
-	   serviceName := strings.Split(paramUrl,"/")[1]
+	   metricType  := strings.Split(paramUrl,"/")[2] // тип метрики
+	   metricName  := strings.Split(paramUrl,"/")[3] // название метрики
+	   metricValue := strings.Split(paramUrl,"/")[4] // значение метрики
 	   
 	   if serviceName != "update" {
 		
               http.Error(w, "service not found", http.StatusNotFound)
               return
 	   }
+	   
+	   // При попытке передать запрос без имени метрики возвращать http.StatusNotFound.
+           if metricName == nil {
+         
+            http.Error(w, "mertic name is empty", http.StatusNotFound)
+         
+           return
+        }
+	   // При попытке передать запрос с некорректным значением возвращать http.StatusBadRequest. 
+        if _, err := strconv.Atoi(metricValue); err == nil {
+		
+         http.Error(w, "mertic value is incorrect", http.StatusBadRequest)
+         
+         return
+      }
 	   
    /*
      tp := r.URL // param type
@@ -48,23 +68,11 @@ func updateCounter(w http.ResponseWriter, r *http.Request){
       
       nm, err := r.URL.Query().Get("nm") // param name
       
-      // При попытке передать запрос без имени метрики возвращать http.StatusNotFound.
-      if err != nil {
-         
-         http.Error(w, "mertic name is empty", http.StatusNotFound)
-         
-         return
-      }
+      
       
      vl, err := r.URL.Query().Get("vl") // param value
      
-      // При попытке передать запрос с некорректным значением возвращать http.StatusBadRequest. 
-     if err != nil {
-         
-         http.Error(w, "mertic value is incorrect", http.StatusBadRequest)
-         
-         return
-      }
+      
       
      // При успешном приёме возвращать http.StatusOK.
      */ 
