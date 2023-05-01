6a4b4a9dfe6a8934e5760f73e824cad8e033ddce
tituscesare <47405702+tituscesare@users.noreply.github.com>
2023-05-01 13:36:30 +0300

    Update wb.go

--- internal/wb/wb.go
+++ internal/wb/wb.go
@@ -60,7 +60,8 @@ func updateCounter(w http.ResponseWriter, r *http.Request){
      */ 
       	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "text/plain")
-	w.Header().Set("Params", tp)   
+
+	w.Write([]byte("blabla\n"))    
          
    }     
 }
