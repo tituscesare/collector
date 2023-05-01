5413ae447367b7d4e8f62597e3f88a92560d566f
tituscesare <47405702+tituscesare@users.noreply.github.com>
2023-05-01 15:09:54 +0300

    Update wb.go

--- internal/wb/wb.go
+++ internal/wb/wb.go
@@ -41,21 +41,21 @@ func updateCounter(w http.ResponseWriter, r *http.Request){
 	   
 	   // При попытке передать запрос с некорректным типом метрики возвращать http.StatusBadRequest. 
       
-      if metricType == "" {
+      if metricType == " " {
          
          http.Error(w, "mertic type is incorrect", http.StatusBadRequest)
          
          return
       }
 	   // При попытке передать запрос без имени метрики возвращать http.StatusNotFound.
-           if metricName == "" {
+           if metricName == " " {
          
             http.Error(w, "mertic name is empty", http.StatusNotFound)
          
            return
         }
 	   // При попытке передать запрос с некорректным значением возвращать http.StatusBadRequest. 
-        if _, err := strconv.Atoi(metricValue); err == nil {
+        if metricValue == " " {
 		
          http.Error(w, "mertic value is incorrect", http.StatusBadRequest)
          
@@ -65,7 +65,8 @@ func updateCounter(w http.ResponseWriter, r *http.Request){
      // При успешном приёме возвращать http.StatusOK.
       	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "text/plain")
-         
+        
+	//fmt.Fprintf(w, 
    }     
 }
 
