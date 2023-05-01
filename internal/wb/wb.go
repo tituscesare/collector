c25e8dc02cac336178d5782b724a48c68d7910eb
tituscesare <47405702+tituscesare@users.noreply.github.com>
2023-05-01 18:21:38 +0300

    Update wb.go

--- internal/wb/wb.go
+++ internal/wb/wb.go
@@ -6,7 +6,7 @@ import (
    "log" 
    "strings"	 
    "fmt"	
-   //"strconv"
+   "strconv"
    "reflect"	
    "net/http"
 	
@@ -34,55 +34,35 @@ func updateCounter(w http.ResponseWriter, r *http.Request){
    
    } else {
 	   
-	   paramUrl := r.URL.RequestURI()
-	  
-	   serviceName := strings.Split(paramUrl,"/")[1] // имя сервиса
-	   
+	   paramsURI := r.URL.RequestURI()
+
 	   metricType  := strings.Split(paramUrl,"/")[2] // тип метрики      -- только строка
 	   metricName  := strings.Split(paramUrl,"/")[3] // название метрики -- только строка
 	   metricValue := strings.Split(paramUrl,"/")[4] // значение метрики -- только не строка 
 	   
-	   if serviceName != "update" {
-		
-              http.Error(w, "service not found", http.StatusNotFound)
-              return
-	   }
-	   
-	   
-	   fmt.Println(reflect.TypeOf(metricType))
-	   fmt.Println(reflect.TypeOf(metricName))
-	   fmt.Println(reflect.TypeOf(metricValue))
-	   
-	   
-	   // При попытке передать запрос с некорректным типом метрики возвращать http.StatusBadRequest. 
-      
-      if metricType == " " {
-         
-         http.Error(w, "mertic type is incorrect", http.StatusBadRequest)
-         
-         return
-      }
-	   // При попытке передать запрос без имени метрики возвращать http.StatusNotFound.
-           if metricName == " " {
-         
-            http.Error(w, "mertic name is empty", http.StatusNotFound)
-         
-           return
-        }
-	   // При попытке передать запрос с некорректным значением возвращать http.StatusBadRequest. 
-        if metricValue == " " {
-		
-         http.Error(w, "mertic value is incorrect", http.StatusBadRequest)
-         
-         return
-	}
-      
-     // При успешном приёме возвращать http.StatusOK.
-      	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "text/plain")
-
-   }     
-}
+	   for i, v := range strings.Split(paramUrl,"/"){
+		   switch i {
+		        case 2:
+			   // При попытке передать запрос с некорректным типом метрики возвращать http.StatusBadRequest.
+			   http.Error(w, "mertic type is incorrect", http.StatusBadRequest)
+			   return
+			case 3:
+			   // При попытке передать запрос без имени метрики возвращать http.StatusNotFound.
+			   http.Error(w, "mertic name is empty", http.StatusNotFound)
+			   return
+		        case 4:
+			   if s, err := strconv.ParseFloat(v, 64); err != nil {
+			   // При попытке передать запрос с некорректным значением возвращать http.StatusBadRequest. 
+			   http.Error(w, "mertic value is incorrect", http.StatusBadRequest)  
+			   return
+			  }
+		        default:
+			   w.WriteHeader(http.StatusOK)
+	                   w.Header().Set("Content-Type", "text/plain")
+		   }	   
+		 }
+             }     
+          }
 
 func HandleRequests() {
 
